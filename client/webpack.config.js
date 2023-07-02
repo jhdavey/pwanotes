bafbe6139b5e4113ac0a2b9f5b0f29a48f317883
jhdavey <jhdavey5182@gmail.com>
2023-07-02 11:36:32 -0400

    Added workbox plugins, css loaders, and babel to webpack in webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,53 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const { GenerateSW } = require('workbox-webpack-plugin');
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add and configure workbox plugins for a service worker and manifest file. DONE
+// TODO: Add CSS loaders and babel to webpack. DONE
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: './js/index.js',
+      install: './js/install.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new WorkboxWebpackPlugin.InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "sw.js"
+      }),
+      new MiniCssExtractPlugin(),
+      new WorkboxWebpackPlugin.GenerateSW()
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
